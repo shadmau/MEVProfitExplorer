dd3d10c47b6a9c3784d9bbbb8ab74d892f2e358f
done999 <done998@protonmail.com>
2023-01-01 02:23:52 +0100

    profit per block added

--- internal/dashboard/textDashboard.go
+++ internal/dashboard/textDashboard.go
@@ -9,7 +9,7 @@ import (
 	"github.com/shadmau/MEVProfitExplorer/pkg/ethereum"
 )
 
-const DEFAULT_BLOCKS = 500
+const DEFAULT_BLOCKS = 216000 //30 days
 
 func ShowTextDashboard(startBlock uint, endBlock uint, rpc string, ignoreWithdrawls bool) {
 	err := godotenv.Load("../../.env")
@@ -38,4 +38,37 @@ func ShowTextDashboard(startBlock uint, endBlock uint, rpc string, ignoreWithdra
 		startBlock = endBlock - DEFAULT_BLOCKS
 	}
 
+	//	startBlock = 16306260
+	//	endBlock = 16307319
+	//	totalProfit := big.NewInt(0)
+	for i := startBlock; i < endBlock; i++ {
+		/*
+				profitForBlock := ethereum.EthProfitForBlock(client, i, "xxx")
+
+				totalProfit.Add(totalProfit, &profitForBlock)
+			   fmt.Printf("Analyzing Block %d/%d (%d%%);  Profit: %s ETH \r", i, endBlock, (i-startBlock)*100/(endBlock-startBlock), ethereum.ConvertWEIToETH(totalProfit))
+			   time.Sleep(time.Second)
+		*/
+	}
+
 }
+
+/*
+
+Trading Profit Dashboard
+
+Date: 2022-12-31
+
+Average Profit per Day: 123.45
+Average Profit per Week: 678.90
+Average Profit per Month: 2987.65
+
+Maximum Profit per Day: 456.78
+Maximum Profit per Week: 2345.67
+Maximum Profit per Month: 9876.54
+
+
+
+
+
+*/
--- pkg/ethereum/utils.go
+++ pkg/ethereum/utils.go
@@ -2,8 +2,11 @@ package ethereum
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -18,4 +21,36 @@ func GetCurrentBlockNumber(client *ethclient.Client) uint {
 		log.Fatal("Could not get Blocknumber!")
 	}
 	return uint(blocknumber)
+}
+
+func EthProfitForBlock(client *ethclient.Client, blocknumber uint, walletAddressString string) big.Int {
+	blocknumberBefore := int64(blocknumber - 1)
+	blocknumberAfter := int64(blocknumber)
+
+	balanceBefore, err := client.BalanceAt(context.Background(), common.HexToAddress(walletAddressString), big.NewInt(blocknumberBefore))
+	if err != nil {
+		log.Fatal("Could not load balance for " + walletAddressString + " for block " + fmt.Sprint(blocknumber))
+	}
+	balanceAfter, err := client.BalanceAt(context.Background(), common.HexToAddress(walletAddressString), big.NewInt(blocknumberAfter))
+	if err != nil {
+		log.Fatal("Could not load balance for " + walletAddressString + " for block " + fmt.Sprint(blocknumber))
+	}
+	result := new(big.Int).Set(balanceAfter).Sub(balanceAfter, balanceBefore)
+
+	//reduce result by TransactionFees
+	return *result
+
+}
+
+// todo implement
+func getTransactionFeesByBlock(client *ethclient.Client, blocknumber uint, walletAddressString string) *big.Int {
+
+	return big.NewInt(0)
+}
+
+func ConvertWEIToETH(wei *big.Int) string {
+	weiFloat := new(big.Float).SetInt(wei)
+	ethFloat := new(big.Float).Quo(weiFloat, big.NewFloat(1e18))
+	eth, _ := ethFloat.Float64()
+	return fmt.Sprintf("%.5f", eth)
 }
